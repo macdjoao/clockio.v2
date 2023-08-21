e7683a7023a783e9a586be6399d7dc5ce22eecf4
João <joao@pop-os.localdomain>
2023-08-21 20:12:59 -0300

    feat: FBV clock (detail) e http_status_code

--- clock/urls.py
+++ clock/urls.py
@@ -2,5 +2,6 @@ from clock import views
 from django.urls import path
 
 urlpatterns = [
-    path('api/home', views.hello_world)
+    path('api/clocks', views.clocks, name='api/clocks'),
+    path('api/clock/<int:id>', views.clock, name='api/clock/id')
 ]
--- clock/views.py
+++ clock/views.py
@@ -1,7 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from clock.models import Clock
+from clock.serializers import ClockSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.status import HTTP_200_OK
 
 
 @api_view()
-def hello_world(request):
-    return Response({"message": "Hello, world!"})
+def clocks(request):
+    clocks = Clock.objects.all()
+    serializer = ClockSerializer(instance=clocks, many=True)
+    return Response(serializer.data, status=HTTP_200_OK)
+
+
+@api_view()
+def clock(request, id):
+    clock = get_object_or_404(Clock, id=id)
+    serializer = ClockSerializer(instance=clock)
+    return Response(serializer.data, status=HTTP_200_OK)
