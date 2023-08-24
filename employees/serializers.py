848655672b36254e5a330c9d7c51d27565c0276a
João Aguiar <macdjoao@gmail.com>
2023-08-24 19:44:54 -0300

    fix: Melhorando legibilidade dos fields de EmployeeOutSerializer

--- employees/serializers.py
+++ employees/serializers.py
@@ -5,4 +5,8 @@ from django.contrib.auth.models import User
 class EmployeeOutSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'first_name', 'last_name', 'is_active']
+        fields = ['id',
+                  'email',
+                  'first_name',
+                  'last_name',
+                  'is_active']
