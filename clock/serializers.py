82b2779a48c52ba49341d93b3fd6c8691dfe5ea9
João Aguiar <macdjoao@gmail.com>
2023-08-24 19:23:12 -0300

    feat: ClockOutSerializer (usando Serializer)

--- clock/serializers.py
+++ clock/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from clock.models import Clock
+from employees.serializers import EmployeeOutSerializer
 
 
-class ClockSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Clock
-        fields = '__all__'
+# class ClockOutSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Clock
+#         fields = '__all__'
+#         depth = 1
+
+
+class ClockOutSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    created_at = serializers.DateTimeField()
+    updated_at = serializers.DateTimeField()
+    # employee = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    employee = EmployeeOutSerializer()
+    entry_date = serializers.DateTimeField()
+    out_date = serializers.DateTimeField()
+    description = serializers.CharField(max_length=255)
+    to_correction = serializers.BooleanField()
--- clock/views.py
+++ clock/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from clock.models import Clock
-from clock.serializers import ClockSerializer
+from clock.serializers import ClockOutSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework.status import HTTP_200_OK
 
@@ -9,12 +9,12 @@ from rest_framework.status import HTTP_200_OK
 @api_view()
 def clocks(request):
     clocks = Clock.objects.all()
-    serializer = ClockSerializer(instance=clocks, many=True)
+    serializer = ClockOutSerializer(instance=clocks, many=True)
     return Response(serializer.data, status=HTTP_200_OK)
 
 
 @api_view()
-def clock(request, id):
+def clock(request, id: int):
     clock = get_object_or_404(Clock, id=id)
-    serializer = ClockSerializer(instance=clock)
+    serializer = ClockOutSerializer(instance=clock)
     return Response(serializer.data, status=HTTP_200_OK)
