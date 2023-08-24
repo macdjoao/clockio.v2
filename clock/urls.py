ba7b67ad9117833261505d685e5103ca1d2fd7d7
João Aguiar <macdjoao@gmail.com>
2023-08-24 18:44:05 -0300

    fix: Correção das urls para padrão REST

--- clock/urls.py
+++ clock/urls.py
@@ -2,6 +2,6 @@ from clock import views
 from django.urls import path
 
 urlpatterns = [
-    path('api/clocks', views.clocks, name='api/clocks'),
+    path('api/clock', views.clocks, name='api/clock'),
     path('api/clock/<int:id>', views.clock, name='api/clock/id')
 ]
