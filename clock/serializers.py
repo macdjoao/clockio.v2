d9ec8c8ed1c899740cb0bd46b8b7350d3ee5d3db
João Aguiar <macdjoao@gmail.com>
2023-08-24 19:40:00 -0300

    feat: ClockOutSerializer e EmployeeOutSerializer (usando ModelSerializer)

--- clock/serializers.py
+++ clock/serializers.py
@@ -1,23 +1,29 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
 from clock.models import Clock
-from employees.serializers import EmployeeOutSerializer
+# from django.contrib.auth.models import User
+# from employees.serializers import EmployeeOutSerializer
 
 
-# class ClockOutSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Clock
-#         fields = '__all__'
-#         depth = 1
+class ClockOutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Clock
+        fields = ['id',
+                  'employee_id',
+                  'entry_date',
+                  'out_date',
+                  'description',
+                  'to_correction',
+                  'updated_at',
+                  'created_at']
 
 
-class ClockOutSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    created_at = serializers.DateTimeField()
-    updated_at = serializers.DateTimeField()
-    # employee = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
-    employee = EmployeeOutSerializer()
-    entry_date = serializers.DateTimeField()
-    out_date = serializers.DateTimeField()
-    description = serializers.CharField(max_length=255)
-    to_correction = serializers.BooleanField()
+# class ClockOutSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     created_at = serializers.DateTimeField()
+#     updated_at = serializers.DateTimeField()
+#     # employee = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+#     employee = EmployeeOutSerializer()
+#     entry_date = serializers.DateTimeField()
+#     out_date = serializers.DateTimeField()
+#     description = serializers.CharField(max_length=255)
+#     to_correction = serializers.BooleanField()
--- employees/serializers.py
+++ employees/serializers.py
@@ -2,16 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 
-# class EmployeeOutSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ['id', 'first_name', 'last_name', 'email', 'is_active']
-
-
-class EmployeeOutSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    username = serializers.CharField()
-    email = serializers.EmailField()
-    first_name = serializers.CharField()
-    last_name = serializers.CharField()
-    is_active = serializers.BooleanField()
+class EmployeeOutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name', 'is_active']
