52ded05f37626779322f52d231c86422fc38ae4c
João Aguiar <macdjoao@gmail.com>
2023-08-26 00:39:20 -0300

    feat: Refatoração do endpoint clock

--- clock/views.py
+++ clock/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from clock.services import ClockService
+from rest_framework.parsers import JSONParser
 
 service = ClockService()
 
@@ -9,7 +10,11 @@ def clocks(request):
     return service.read_list()
 
 
+# TODO: Method POST Not Allowed
 @api_view(http_method_names=['GET', 'POST', 'PATCH', 'DELETE'])
-def clock(request, id: int):
+def clock(request, id: int = None):
     if request.method == 'GET':
         return service.read_one(id=id)
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        return service.create(data=data)
