a80287b1246ddd94d1dd7a15151ed6c71df9d0be
João Aguiar <macdjoao@gmail.com>
2023-08-26 00:38:11 -0300

    feat: ClockInSerializer

--- clock/serializers.py
+++ clock/serializers.py
@@ -16,3 +16,19 @@ class ClockOutSerializer(serializers.ModelSerializer):
                   'employee',
                   'updated_at',
                   'created_at']
+
+
+class ClockInSerializer(serializers.Serializer):
+
+    # TODO: Criar um serializer EmployeeIn
+
+    employee_id = serializers.IntegerField(required=True)
+    entry_date = serializers.DateTimeField(required=True)
+    out_date = serializers.DateTimeField(required=True)
+    description = serializers.CharField(required=False)
+
+    def validate(self, data):
+        if data['entry_date'] > data['out_date']:
+            raise serializers.ValidationError(
+                'out_date must occur after entry_date')
+        return data
