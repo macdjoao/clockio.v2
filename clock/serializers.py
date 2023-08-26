1d038377fc64a97b6f495f21e200c02c3977e92b
João Aguiar <macdjoao@gmail.com>
2023-08-25 23:33:49 -0300

    fix: Limpando arquivo

--- clock/serializers.py
+++ clock/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from clock.models import Clock
-# from django.contrib.auth.models import User
 from employees.serializers import EmployeeOutSerializer
 
 
@@ -17,15 +16,3 @@ class ClockOutSerializer(serializers.ModelSerializer):
                   'employee',
                   'updated_at',
                   'created_at']
-
-
-# class ClockOutSerializer(serializers.Serializer):
-#     id = serializers.IntegerField()
-#     created_at = serializers.DateTimeField()
-#     updated_at = serializers.DateTimeField()
-#     # employee = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
-#     employee = EmployeeOutSerializer()
-#     entry_date = serializers.DateTimeField()
-#     out_date = serializers.DateTimeField()
-#     description = serializers.CharField(max_length=255)
-#     to_correction = serializers.BooleanField()
