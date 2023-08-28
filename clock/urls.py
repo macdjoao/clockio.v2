3da7b3644839316c5ab4dff33c914e15d47c2fb2
João Aguiar <macdjoao@gmail.com>
2023-08-28 18:34:11 -0300

    fix: Correção das urls

--- clock/urls.py
+++ clock/urls.py
@@ -2,6 +2,9 @@ from clock import views
 from django.urls import path
 
 urlpatterns = [
-    path('api/clock', views.clocks, name='api-clocks'),
-    path('api/clock/<int:id>', views.clock, name='api-clock-id')
+    path('api/get_clocks', views.get_clocks, name='get-clocks'),
+    path('api/get_clock/<int:id>', views.get_clock, name='get-clock'),
+    path('api/post_clock', views.post_clock, name='post-clock'),
+    path('api/patch_clock/<int:id>', views.update_clock, name='patch-clock'),
+    path('api/delete_clock/<int:id>', views.delete_clock, name='delete-clock'),
 ]
