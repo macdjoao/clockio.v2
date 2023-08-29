1385b3d8b70200963d92d09d21cc48377689e654
João Aguiar <macdjoao@gmail.com>
2023-08-29 20:00:20 -0300

    feat: Refatorando ClockInSerializer (Serializer -> ModelSerializer)

--- clock/serializers.py
+++ clock/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+
 from clock.models import Clock
 from employees.serializers import EmployeeOutSerializer
 
@@ -18,20 +19,40 @@ class ClockOutSerializer(serializers.ModelSerializer):
                   'created_at']
 
 
-class ClockInSerializer(serializers.Serializer):
-
-    # TODO: Criar um serializer EmployeeIn
-
-    employee_id = serializers.IntegerField(required=True)
-    entry_date = serializers.DateTimeField(required=True)
-    out_date = serializers.DateTimeField(required=True)
-    description = serializers.CharField(required=False)
+class ClockInSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Clock
+        fields = ['employee',
+                  'entry_date',
+                  'out_date',
+                  'description']
 
     def validate(self, data):
         if data['entry_date'] > data['out_date']:
-            raise serializers.ValidationError(
-                'out_date must occur after entry_date')
+            raise serializers.ValidationError({
+                'out_date': ['out_date must occur after entry_date']
+            })
         return data
 
-    def create(self, validated_data):
-        return Clock.objects.create(**validated_data)
+
+# class ClockInSerializer(serializers.Serializer):
+
+#     employee_id = serializers.IntegerField(required=True)
+#     entry_date = serializers.DateTimeField(required=True)
+#     out_date = serializers.DateTimeField(required=True)
+#     description = serializers.CharField(required=False)
+
+#     def create(self, validated_data):
+#         return Clock.objects.create(**validated_data)
+
+#     def validate(self, data):
+#         if data['entry_date'] > data['out_date']:
+#             raise serializers.ValidationError({
+#                 'out_date': ['out_date must occur after entry_date']
+#             })
+#         return data
+
+#     def validate_employee_id(self, value):
+#         if value == 0:
+#             raise serializers.ValidationError('employee_id cannot be 0')
+#         return value
