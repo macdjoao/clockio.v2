7a30a13440e49f83c1e7a20512b7563e60a0eaf1
João Aguiar <macdjoao@gmail.com>
2023-08-30 19:56:02 -0300

    feat: Adicionando enpoint DELETE

--- clock/views.py
+++ clock/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_list_or_404, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED, HTTP_200_OK
+from rest_framework.status import HTTP_201_CREATED, HTTP_200_OK, HTTP_204_NO_CONTENT
 
 from clock.models import Clock
 from clock.serializers import ClockOutSerializer, ClockInSerializer
@@ -9,14 +9,20 @@ from clock.serializers import ClockOutSerializer, ClockInSerializer
 
 @api_view(['GET', 'PATCH', 'DELETE'])
 def clock_with_id(request, id: int):
+    clock = get_object_or_404(Clock, id=id)
     if request.method == 'GET':
-        clock = get_object_or_404(Clock, id=id)
         serializer = ClockOutSerializer(instance=clock)
         return Response(serializer.data, status=HTTP_200_OK)
     if request.method == 'PATCH':
+        # TODO
+        # serializer = ClockInSerializer(
+        #     instance=clock, data=request.data, partial=True)
+        # serializer.is_valid(raise_exception=True)
+        # serializer.save()
         return Response({'method': 'patch'})
     if request.method == 'DELETE':
-        return Response({'method': 'delete'})
+        clock.delete()
+        return Response({'detail': 'Clock successfully deleted'}, status=HTTP_204_NO_CONTENT)
 
 
 @api_view(['GET', 'POST'])
