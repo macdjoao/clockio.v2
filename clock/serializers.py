5e4076034c520725fe92fb18ec93293607418d78
João Aguiar <macdjoao@gmail.com>
2023-09-06 23:40:29 -0300

    fix: Refactoring serializer

--- clock/serializers.py
+++ clock/serializers.py
@@ -1,4 +1,6 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 from clock.models import Clock
 from employees.serializers import EmployeeOutSerializer
